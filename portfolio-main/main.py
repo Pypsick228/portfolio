a5b684d060ace79700e62c366746609b69f5fc26
Pypsick <132742134+Pypsick228@users.noreply.github.com>
2023-08-31 18:37:55 +0400

    Add files via upload

---
+++ portfolio-main/main.py
@@ -0,0 +1,39 @@
+#Импорт
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+#Запуск страницы с контентом
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+#Динамичные скиллы
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    button_discord = request.form.get('button_discord')
+    button_html = request.form.get("button_html")
+    button_db = request.form.get("button_db")
+    return render_template('index.html', button_python=button_python, button_discord=button_discord, button_html=button_html, button_db=button_db)
+    
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    text = request.form['text']
+    email = request.form["email"]
+    with open('form.txt', 'a',) as f: 
+        f.write(text + " " + email + " " + '\n')
+    return render_template('form_result.html', 
+                           text=text,
+                            email=email
+                           )
+if __name__ == "__main__":
+    app.run(debug=True)
